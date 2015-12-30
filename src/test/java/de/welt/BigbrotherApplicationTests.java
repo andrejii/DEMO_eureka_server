e8aa2c3c242ba5456639c46c24c0d9b3dffb887d
Andrej Ivanov <andrej.ivanov@weltn24.de>
2015-12-30 18:02:16 +0100

    Initial commit

---
+++ src/test/java/de/welt/BigbrotherApplicationTests.java
@@ -0,0 +1,16 @@
+package de.welt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BigbrotherApplication.class)
+public class BigbrotherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
